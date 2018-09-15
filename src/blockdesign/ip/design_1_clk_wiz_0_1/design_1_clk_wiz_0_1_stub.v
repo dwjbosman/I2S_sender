a13dcc854208658e5454c59fdb47b92cff8b983b
Dinne Bosman <dwjbosman@yahoo.com>
2018-09-15 23:28:00 +0200

    blockdesign added

---
+++ src/blockdesign/ip/design_1_clk_wiz_0_1/design_1_clk_wiz_0_1_stub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
+// Date        : Sat Sep 15 23:16:43 2018
+// Host        : dinne-Aspire-VN7-593G running 64-bit Ubuntu 16.04.4 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/dinne/Xilinx/projects/I2S_sender/vivado_project/vivado_project.srcs/sources_1/bd/design_1/ip/design_1_clk_wiz_0_1/design_1_clk_wiz_0_1_stub.v
+// Design      : design_1_clk_wiz_0_1
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tcsg324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module design_1_clk_wiz_0_1(clk_out1, clk_out2, reset, locked, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_out2,reset,locked,clk_in1" */;
+  output clk_out1;
+  output clk_out2;
+  input reset;
+  output locked;
+  input clk_in1;
+endmodule
---
+++ src/blockdesign/ip/design_1_clk_wiz_0_1/design_1_clk_wiz_0_1_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
+-- Date        : Sat Sep 15 23:16:43 2018
+-- Host        : dinne-Aspire-VN7-593G running 64-bit Ubuntu 16.04.4 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/dinne/Xilinx/projects/I2S_sender/vivado_project/vivado_project.srcs/sources_1/bd/design_1/ip/design_1_clk_wiz_0_1/design_1_clk_wiz_0_1_stub.vhdl
+-- Design      : design_1_clk_wiz_0_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity design_1_clk_wiz_0_1 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_out2 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end design_1_clk_wiz_0_1;
+
+architecture stub of design_1_clk_wiz_0_1 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_out2,reset,locked,clk_in1";
+begin
+end;
